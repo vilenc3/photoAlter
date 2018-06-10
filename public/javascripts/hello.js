823650cdc5ec0d0098e331010b12fe33e8885d2f
vilenc3 <vilence@interia.pl>
2018-06-10 13:00:03 +0200

    revert changes

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,15 +1,3 @@
 if (window.console) {
-    console.log("Welcome to your Play application's JavaScript! C'mon work!");
+  console.log("Welcome to your Play application's JavaScript!");
 }
-$(function () {
-    if (window.location.href != "http://localhost:9000/home" && window.location.href != "http://localhost:9000/register" && window.location.href != "http://localhost:9000/login") {
-        console.log("Logged in");
-        $(".not_logged_in").hide();
-        $(".logged_in").show();
-    }
-    else {
-        console.log("Not logged in");
-        $(".not_logged_in").show();
-        $(".logged_in").hide();
-    }
-});
